c699f25bccab114a8e81c4411597e023b60cde79
Galen Erickson <gkerickson1@gmail.com>
2020-12-31 17:20:16 -0500

    Got schools and success

--- app/src/main/java/galen/nycschools/MainActivity.java
+++ app/src/main/java/galen/nycschools/MainActivity.java
@@ -3,10 +3,18 @@ package galen.nycschools;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     private SchoolRequestHandler requestHandler;
+    private SchoolRequestHandler.SuccessHandler successHandler = new SchoolRequestHandler.SuccessHandler() {
+        @Override
+        public void onSuccess(School[] schools) {
+            Log.d(TAG, "Got this many schools: " + schools.length);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +22,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         requestHandler = new SchoolRequestHandler(this);
 
-        requestHandler.getSchoolInfo();
+        requestHandler.getSchoolInfo(successHandler);
     }
 }
--- app/src/main/java/galen/nycschools/SchoolRequestHandler.java
+++ app/src/main/java/galen/nycschools/SchoolRequestHandler.java
@@ -4,30 +4,33 @@ import android.content.Context;
 import android.util.Log;
 
 import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class SchoolRequestHandler {
-    RequestQueue queue;
-
     private static final String TAG = "SchoolRequestHandler";
     private static final int MY_SOCKET_TIMEOUT_MS = 10000;
     private static final String NYC_SCHOOLS_ENDPOINT =
             "https://data.cityofnewyork.us/resource/s3k6-pzi2.json";
     private static final String SCHOOL_QUERY_STRING = "SELECT school_name, neighborhood, finalgrades";
-
     private static final DefaultRetryPolicy defaultRetryPolicy = new DefaultRetryPolicy(
             MY_SOCKET_TIMEOUT_MS,
             DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
             DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+
+    RequestQueue queue;
+
+    SuccessHandler successHandler;
+
+    public interface SuccessHandler {
+        public void onSuccess(School[] schools);
+    }
 
     public SchoolRequestHandler(Context context) {
         queue = Volley.newRequestQueue(context);
@@ -40,12 +43,13 @@ public class SchoolRequestHandler {
             numberOfSchools = Math.min(numberOfSchools, 10);
             JSONObject school;
             String schoolName;
-            String[] schools = new String[response.length()];
+            School[] schools = new School[response.length()];
             for(int i = 0; i < numberOfSchools; i++) {
                 school = response.optJSONObject(i);
                 schoolName = school.optString("school_name");
-                Log.d(TAG, schoolName);
+                schools[i] = new School(schoolName);
             }
+            successHandler.onSuccess(schools);
         }
     };
 
@@ -67,25 +71,14 @@ public class SchoolRequestHandler {
         }
     };
 
-    protected void getSchoolInfo() {
-        Log.d(TAG, NYC_SCHOOLS_ENDPOINT);
-        StringRequest request = new StringRequest(
-                Request.Method.GET,
-                NYC_SCHOOLS_ENDPOINT,
-                listener,
-                errorListener
-        );
-
-        request.setRetryPolicy(defaultRetryPolicy);
-
+    protected void getSchoolInfo(SuccessHandler successHandler) {
+        this.successHandler = successHandler;
         JsonArrayRequest jsonRequest = new JsonArrayRequest(
                 NYC_SCHOOLS_ENDPOINT,
                 jsonListener,
                 errorListener
         );
         jsonRequest.setRetryPolicy(defaultRetryPolicy);
-
-        queue.add(request);
         queue.add(jsonRequest);
     }
 }
